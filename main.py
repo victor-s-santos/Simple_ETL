f77e4bfcb5059628bc5ecd995ca6f7ab9f5e4f49
Victor <victor.santos.cd@gmail.com>
2023-10-06 20:37:49 -0300

    (main.py): Create a very simple config to insert value in mongodb

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from pymongo import MongoClient
 from decouple import config
 
-credentials = {
-    "user": config("MONGO_USERNAME"),
-    "password": config("MONGO_PASSWORD")
-}
+credentials = {"user": config("MONGO_USERNAME"), "password": config("MONGO_PASSWORD")}
 
-connection_config = f"mongodb://{credentials['user']}:{credentials['password']}@localhost:27017/"
+connection_config = (
+    f"mongodb://{credentials['user']}:{credentials['password']}@localhost:27017/"
+)
 my_val = {"Key": "Value", "AnotherKey": "AnotherValue"}
-
 
 if __name__ == "__main__":
     myclient = MongoClient(connection_config)
     my_db = myclient["DATABASE"]
-
     my_collection = my_db["COLLECTION"]
-
     my_collection.insert_one(my_val)
