cdf6aa41fe4662b4d7cd43ae0441623f32fa28d0
Victor <victor.santos.cd@gmail.com>
2023-10-08 13:23:21 -0300

    (main.py): Use the handle_csv in the main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from decouple import config
 from mongo_db.mongo_connection import Mongo
-
+from handle_csv.main import generate_list_of_dictionaries
+import os
 
 if __name__ == "__main__":
     credentials = {
@@ -15,4 +16,9 @@ if __name__ == "__main__":
     value = {"key": "Value", "AnotherKey": "AnotherValue"}
     my_db = mongo_obj.get_connection()
     my_collection = my_db["NEW_COLLECTION"]
-    my_collection.insert_one(value)
+    file_name = "FastFoodNutritionMenu.csv"
+    path_of_csv_file = f"{os.getcwd()}/input_files/{file_name}"
+    list_of_values = generate_list_of_dictionaries(path_of_csv_file)
+    for value in list_of_values:
+        my_collection.insert_one(value)
+    print(f"{len(list_of_values)} records has been inserted!")
