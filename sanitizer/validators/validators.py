d23048035b47d93bf97efd7c761d07359ec550ba
Victor <victor.santos.cd@gmail.com>
2023-10-08 20:18:45 -0300

    (sanitizer): Create the module sanitizer

---
+++ sanitizer/validators/validators.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+
+def validate_null_in_number_field(value: Any) -> int | Any:
+    """Validate null values in numerical fields
+
+    Args:
+        value (Any): The given field that can be a numerical field or not
+
+    Returns:
+        int|Any: returns 0 if the given value is null otherwise returns the given value.
+    """
+    if not value:
+        return 0
+    return value
